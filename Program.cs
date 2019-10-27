f00402de951e6ba85ed984798dc3c8de8af87bf2
Richard <richardwjmorrow@gmail.com>
2019-10-27 14:06:45 -0700

    created the alarm class and reorganized the alarm code in hope that user input will become a thing in the future.

--- Program.cs
+++ Program.cs
@@ -11,55 +11,17 @@ namespace ClassBreakAlarmClock
 
         static void Main()
         {
-            Alarm();
+            List<string> breakSounds = new List<string>() { @"music\bells-tibetan-daniel_simon.wav", @"music\old-fashioned-school-bell-daniel_simon.wav" };
+            List<string> warningSounds = new List<string>() { @"music\front-desk-bells-daniel_simon.wav", @"music\service-bell_daniel_simion.wav" };
+            
+            string breaksTest = "14:02"; string warningsTest = "14:04";
+
+            List<string> breaks = new List<string>() { "9:30", "11:30", "14:15", "16:00", breaksTest };
+            List<string> warnings =new List<string>() { "9:25", "11:25", "14:10", "15:55", warningsTest };
+
+
+            Alarm.alarm(breaks, breakSounds, ConsoleColor.Green ,warnings,warningSounds,ConsoleColor.Red);
         }
-        static void Alarm()
-        {
-            List<string> breakAlarms = new List<string>() { @"music\bells-tibetan-daniel_simon.wav", @"music\old-fashioned-school-bell-daniel_simon.wav" };
-            List<string> warningAlarms = new List<string>() { @"music\front-desk-bells-daniel_simon.wav", @"music\service-bell_daniel_simion.wav" };
 
-            string breaksTest = "17:34"; string warningsTest = "00:00";
-
-            string[] breaks = { "9:30", "11:30", "14:15", "16:00", breaksTest };
-            string[] warnings = { "9:25", "11:25", "14:10", "15:55", warningsTest };
-
-            while (true)
-            {
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine(DateTime.Now.ToString("H:mm:ss"));
-                Console.SetCursorPosition(0, 0);
-                Console.CursorVisible = false;
-
-                string buffer = DateTime.Now.ToString("H:mm"); //actual breaks
-                foreach (string time in breaks)
-                {
-                    if (buffer.Contains(time))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("It is break time.");
-
-                        Sounds.MusicPlayer(breakAlarms);
-
-                        Thread.Sleep(60000);
-                        Console.Clear();
-                    }
-                }
-                foreach (string time in warnings) //warnings
-                {
-                    if (buffer.Contains(time))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Warning!! Five minutes until break time.");
-
-                        Sounds.MusicPlayer(warningAlarms);
-
-                        Thread.Sleep(60000);
-                        Console.Clear();
-                    }
-                }
-            }
-
-
-        }
     }
 }
