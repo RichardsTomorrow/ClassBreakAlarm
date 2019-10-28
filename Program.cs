0de12d8280516b2028482a081223d77d855a1b19
Richard <richardwjmorrow@gmail.com>
2019-10-28 08:16:23 -0700

    made a quick build. I guess this is v 1

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace ClassBreakAlarmClock
             List<string> breakSounds = new List<string>() { @"music\bells-tibetan-daniel_simon.wav", @"music\old-fashioned-school-bell-daniel_simon.wav" };
             List<string> warningSounds = new List<string>() { @"music\front-desk-bells-daniel_simon.wav", @"music\service-bell_daniel_simion.wav" };
             
-            string breaksTest = "14:02"; string warningsTest = "14:04";
+            string breaksTest = "00:00"; string warningsTest = "00:00";
 
             List<string> breaks = new List<string>() { "9:30", "11:30", "14:15", "16:00", breaksTest };
             List<string> warnings =new List<string>() { "9:25", "11:25", "14:10", "15:55", warningsTest };
